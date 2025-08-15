a1041bae9255c29e6df96bd4fae12543699ae21c
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-08-14 23:20:56 -0300

    Aula e exercicios

---
+++ aulas/aula07.py
@@ -0,0 +1,8 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+
+soma = n1 + n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print('A soma é {}, \no produto é {} \ne a divisão é {}'.format(soma, multiplicacao, divisao))
---
+++ exercicios/ex005.py
@@ -0,0 +1,3 @@
+n = float(input('Digite o seu número: '))
+
+print('Seu número é {}, seu antecessor é {} e seu sucessor é {}'.format((n),(n-1),(n+1)))
